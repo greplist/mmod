7e6045576e43e321b9067e9d0ed4001c00ec2462
Anton Grabko <anton.grabko@synesis.ru>
2017-10-05 13:36:27 +0300

    3: kolmogorov is rady

--- 3.py
+++ 3.py
@@ -24,31 +24,38 @@ def MKM(count, n=30, k=123456789, A0=2):
     return z
 
 
+def build_F(X, Y):
+    N = [0] * len(X)
+    for i, x in enumerate(X):
+        N[i] = sum([1 if y <= x else 0 for y in Y])
+
+    count = len(Y)
+    return list([n * 1.0 / count for n in N])
+
+
 def exp_test(count):
-    Y = list([norm.ppf(x) for x in MKM(count)])
+    Y = list(([norm.ppf(x) for x in MKM(count)]))
     MY, TMY, DY, TDY = M(Y), 0.0, D(Y), norm.var()
     pylab.title('{} numbers. M = {:.4} TM = {:.4} D = {:.4} TD = {:.4}'.format(count, MY, TMY, DY, TDY))
     pylab.hist(Y, normed=1)
 
-    X = list([x * 0.01 for x in xrange(-500, 500)])
+    X = list([x * 0.01 for x in xrange(-400, 400)])
     TY = list([norm.pdf(x) for x in X])
     pylab.plot(X, TY)
 
     pylab.show()
 
-    # lambda(0.05) = 0.89
-    F = [i / float(y) for i, y in enumerate(Y)]
+    F = build_F(X, Y)
     TF = [norm.cdf(x) for x in X]
-    pylab.title('kolmogorov: must be {} < 0.86'.format(1 / math.sqrt(count) * max([abs(f - ft) for f, ft in zip(F, TF)])))
+
+    max_delta = max([abs(f - ft) for f, ft in zip(F, TF)])
+    sigma = math.sqrt(count) * max_delta
+    pylab.title('kolmogorov: max delta = {:.4} sigma = {:.4} < 0.86'.format(max_delta, sigma))
+    pylab.plot(X, F)
+    pylab.plot(X, TF)
 
     pylab.show()
 
 
-# def kolmogorov():
-#     F1_teor = [1 - math.e ** (-l*y1[i]) for i in xrange(len(y1))]
-#     F2_teor = [1 - math.e ** (-l*y2[i]) for i in xrange(len(y2))]
-#     print math.sqrt(n)*max(abs(F2[i] - F2_teor[i]) for i in xrange(len(y2)))
-
-
 if __name__ == '__main__':
-    exp_test(10000)
+    exp_test(1000)
